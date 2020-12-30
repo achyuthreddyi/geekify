09f8e2a270916f2bc51027c250805bd7b62661a6
achyuth reddy <ncsachyuth@gmail.com>
2020-12-30 11:51:48 +0530

    getting the token from the uri and able to store it into the local state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Login from './pages/login/Login'
+import { getTokenFromUrl } from './spotify'
 
 function App () {
+  const [token, setToken] = useState('')
+
+  useEffect(() => {
+    const tokenObj = getTokenFromUrl()
+    window.location.hash = ''
+    const _token = tokenObj.access_token
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log('i have a token ', token)
+  }, [token])
+
   return (
     <div className='app'>
       <Login />
