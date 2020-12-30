be8683612f87c5b20799442351dd2af9edb57822
achyuth reddy <ncsachyuth@gmail.com>
2020-12-30 15:28:29 +0530

    designed the sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarOption from './SidebarOption'
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined'
+import SearchIcon from '@material-ui/icons/Search'
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic'
 
 function Sidebar () {
   return (
     <div className='sidebar'>
-      <h1>sidebar</h1>
+      <h1 className='sidebar__logo'>geekify</h1>
+      <hr />
+
+      <SidebarOption title='Home' Icon={HomeOutlinedIcon} />
+      <SidebarOption title='Search' Icon={SearchIcon} />
+      <SidebarOption title='Your Library' Icon={LibraryMusicIcon} />
+      <br />
+      <strong className='sidebar__title'>PLAYLISTS</strong>
+      <hr />
+      <SidebarOption title='hiphop' />
+      <SidebarOption title='jazz' />
+      <SidebarOption title='carnatic' />
     </div>
   )
 }
